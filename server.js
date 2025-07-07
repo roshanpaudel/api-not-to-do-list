05ece7d23f31cca1ffa080a5392e5949f6e70608
Roshan Paudel <paudelroshan93@gmail.com>
2025-07-07 14:55:26 +1000

    bug fixed

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ import express from "express";
 const PORT = 8000;
 const app = express();
 
+app.use("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "todo",
+  });
+});
 // app.get("/", (req, res) => {
 //   console.log(res);
 // });
 app.listen(PORT, (error) => {
   error
     ? console.log(error)
-    : console.log(`server running at http://localhost/${PORT}`);
+    : console.log(`Server running at http://localhost:${PORT}`);
 });
