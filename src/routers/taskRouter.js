964118f08fb8d1e7d8400c56d5bb1759c7e0d52b
Roshan Paudel <paudelroshan93@gmail.com>
2025-07-10 18:09:27 +1000

    GET,POST and PUT methods defined

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -3,9 +3,28 @@ import express from "express";
 const router = express.Router();
 
 router.all("/", (req, res, next) => {
+  //   res.json({
+  //     status: "success",
+  //     message: "response form all",
+  //   });
+  next();
+});
+router.get("/", (req, res, next) => {
   res.json({
     status: "success",
-    message: "response form all",
+    message: "response form get",
+  });
+});
+router.post("/", (req, res, next) => {
+  res.json({
+    status: "success",
+    message: "response form post",
+  });
+});
+router.put("/", (req, res, next) => {
+  res.json({
+    status: "success",
+    message: "response form put",
   });
 });
 
