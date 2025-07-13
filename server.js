4cc063c7f1dbe6ef0365b67b7d7b30927dbc0e8d
Roshan Paudel <paudelroshan93@gmail.com>
2025-07-13 13:04:39 +1000

    DBconenct function called from serverjs

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use("/api/v1/tasks", taskRouter);
 app.use(morgan("combined"));
 
+//Connect MongoDB
+import { connectMongoDB } from "./src/config/dbConfig.js";
+connectMongoDB();
+
 // app.get("/", (req, res) => {
 //   console.log(res);
 // });
