08cae7316c7e049c63dd1fe77aa45eb7610f88da
Roshan Paudel <paudelroshan93@gmail.com>
2025-07-24 21:31:16 +1000

    env changed

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,10 +1,6 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-dotenv.config(); // Loads variables from .env into process.env
-const user = process.env.MONGO_USER;
-const pass = encodeURIComponent(process.env.MONGO_PASS);
-const mongoURL = `mongodb+srv://${user}:${pass}@online-class-db.ivt39qv.mongodb.net/online_ntdl?retryWrites=true&w=majority&appName=Online-class-db`;
+const mongoURL = process.env.MONGO_URL;
 
 export const connectMongoDB = async () => {
   try {
